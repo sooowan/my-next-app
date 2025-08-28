e34fbc95be41b9917fa09dce52b527dd23fbdf2a
이수완 <>
2025-08-29 05:50:02 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 //app/page.tsx
 
-console.log(process.env.SERVER_ENV_VAR);
-console.log(process.env.NEXT_PUBLIC_CLIENT_ENV_VAR);
+// console.log(process.env.SERVER_ENV_VAR);
+// console.log(process.env.NEXT_PUBLIC_CLIENT_ENV_VAR);
 
 export default function IndexPage() {
 	return (
